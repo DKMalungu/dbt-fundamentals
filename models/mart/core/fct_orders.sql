e6c70da6c7e2fc828e7817244652b030784984ac
Daniel K. Malungu <daniel.malungu@cloudfactory.com>
2022-04-07 13:48:10 +0000

    bug_fix: minor

--- models/mart/core/fct_orders.sql
+++ models/mart/core/fct_orders.sql
@@ -4,6 +4,6 @@ with order_detail as (
            sp.amount as amount
     from {{ref('stg_orders')}} as so
     inner join {{ref('stg_customers')}} as sc on so.customer_id = sc.customer_id
-    inner join {{ref('stg_payments')}} as sp on so.order_id = sc.order_id
+    inner join {{ref('stg_payments')}} as sp on so.order_id = sp.order_id
 )
 select order_id, customer_id, amount from order_detail
