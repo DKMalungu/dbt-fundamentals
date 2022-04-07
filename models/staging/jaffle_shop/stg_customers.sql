8d45d0f21520cfaa50901c89e2b3cbb76f202eb7
danielmalungu <danielkmalungu@gmail.com>
2022-04-07 22:14:13 +0300

    update: configuring dbt source

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -2,6 +2,6 @@ with customer as (
     select id as customer_id,
            first_name,
            last_name
-    from jaffle_shop_customers
+    from {{source('jaffle_shop', 'jaffle_shop_customers')}}
 )
 Select * from customer
