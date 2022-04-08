9f39ec22310975b7ff17389269f2f8471e5069e0
danielmalungu <danielkmalungu@gmail.com>
2022-04-09 01:55:59 +0300

    bug_fix: minor

--- tests/assert_stg_payment_amount_positive.sql
+++ tests/assert_stg_payment_amount_positive.sql
@@ -3,7 +3,7 @@ payments as (
     select * from {{ref('stg_payments')}}
 )
 select order_id,
-       sum(amount) as total_amont
+       sum(amount) as total_amount
 from payments
 group by order_id
 having total_amount < 0
